a75428354461034c6afe3f57aa6d4b53f0206fc1
Osvaldo Calombe <osvaldozamba@gmail.com>
2020-05-20 14:42:27 +0100

    deploy branch

--- libs/dist/js/ajax-requests-doc.js
+++ libs/dist/js/ajax-requests-doc.js
@@ -26,13 +26,13 @@ $(function() {
     function renderUrl(filtered) {
         switch (filtered) {
             case '1':
-                return 'http://localhost/itel/gestoasimaotoco/documento/paroquias/';
+                return 'http://172.16.200.8/documento/paroquias/';
                 break;
             case '2':
-                return 'http://localhost/itel/gestoasimaotoco/documento/provincias/';
+                return 'http://172.16.200.8/documento/provincias/';
                 break;
             case '3':
-                return 'http://localhost/itel/gestoasimaotoco/documento/igrejas/';
+                return 'http://172.16.200.8/documento/igrejas/';
                 break;
             default:
                 break;
